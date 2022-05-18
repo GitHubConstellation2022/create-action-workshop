6f7bdca54a7c4f3dffc288d52bbd835de8fc80e6
Anshul Choudhary <ansh121@github.com>
2022-05-18 12:28:09 +0530

    fix

--- index.js
+++ index.js
@@ -16,15 +16,20 @@ try {
  const pull_request_stats = { open: 0, closed: 0, merged: 0 };
 
  for (const item of issues_and_prs) {
-   if ('pull_request' in item) {
-     if (item.state == 'open') pull_request_stats.open++;
-     else if (item.merged_at == null) pull_request_stats.closed++;
-     else pull_request_stats.merged++;
-   }
-   else {
-     if (item.state == 'open') issue_stats.open++;
-     else issue_stats.closed++;
-   }
+  if('pull_request' in item){
+    if (item.state == 'open') 
+            pull_request_stats.open++;
+    else if (item.pull_request.merged_at == null) 
+            pull_request_stats.closed++;
+    else 
+            pull_request_stats.merged++;
+}
+else{
+    if (item.state == 'open') 
+            issue_stats.open++;
+    else 
+            issue_stats.closed++;
+}
  }
 
  setOutput('pull_request_stats', pull_request_stats);
